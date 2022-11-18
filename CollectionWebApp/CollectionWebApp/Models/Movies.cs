f3b61b3a9f6902ee5c63b90356a74e2d8d5211f4
Nathan Cochnar <nathan.cochnar@doane.edu>
2022-11-18 11:32:06 -0600

    Pushing CollectionWebApp

---
+++ CollectionWebApp/CollectionWebApp/Models/Movies.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Diagnostics.CodeAnalysis;
+using CollectionWebApp.Models;
+
+namespace CollectionWebApp.Models
+{
+    public class Movies
+    {
+        public long Id { get; set; }
+        public string? Name { get; set; }
+        public bool Watched { get; set; }
+    }
+}
+
---
+++ CollectionWebApp/CollectionWebApp/Models/MoviesContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Diagnostics.CodeAnalysis;
+using CollectionWebApp.Models;
+
+
+namespace CollectionWebApp.Models
+{
+    public class MoviesContext : DbContext
+    {
+        public MoviesContext(DbContextOptions<MoviesContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Movies> Movies { get; set; } = null!;
+    }
+}
